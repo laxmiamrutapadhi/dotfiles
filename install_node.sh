b22c8c124d3bcf12eadc80af5c08762e2b476739
Laxmi Amruta Padhi <164632284+laxmiamrutapadhi@users.noreply.github.com>
2025-10-09 06:05:57 +0530

    Create install_node.sh

---
+++ install_node.sh
@@ -0,0 +1,11 @@
+if command -v node >/dev/null 2>&1; then
+    exit 0
+fi
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+nvm install --lts
